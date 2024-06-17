7d48f705bb6e89304ace18bd5a1f1ce432e7f59e
LauYzz <Liuyuanzhe2002@gmail.com>
2024-06-18 01:05:04 +0800

    refactor project

--- app/src/main/java/com/example/freshkeeper/model/InfoItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.freshkeeper.model
-
-class InfoItem(
-    var name: String,
-    var value: String,
-    var image: Int
-)
--- app/src/main/java/com/example/freshkeeper/model/NavItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.freshkeeper.model
-
-class NavItem(
-    var name: String,
-    var image: Int
-)
